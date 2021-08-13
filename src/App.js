e2067c76c2273f6ab2de9cb21f3817700c37f739
Dipti Rani <gdipti642@gmail.com>
2021-08-13 16:21:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: 0
+    }
+    this.handleInput = this.handleInput.bind(this);
+  }
+  handleInput(key){
+    let newInput = document.getElementById(key).textContent;
+    console.log(newInput);
+    // console.log(this.state.input);
+    this.setState({
+      //  input: document.getElementById(key).textContent
+      input: [...this.state.input, newInput]
+    })
+
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <div className="calculator">
+          <div class="formulaScreen"></div>
+          <div class="outputScreen" id="display">{this.state.input}</div>
+          
+          <div id="number-plate">
+            <button id="clear" className="jumbo">AC</button>
+            <button id="divide" onClick={() => this.handleInput('divide')}>/</button>
+            <button id="multiply" onClick={() => this.handleInput('multiply')}>x</button>
+            <button id="seven" onClick={() => this.handleInput('seven')}>7</button>
+            <button id="eight" onClick={() => this.handleInput('eight')}>8</button>
+            <button id="nine" onClick={() => this.handleInput('nine')}>9</button>
+            <button id="subtract" onClick={() => this.handleInput('subtract')}>-</button>
+            <button id="four" onClick={() => this.handleInput('four')}>4</button>
+            <button id="five" onClick={() => this.handleInput('five')}>5</button>
+            <button id="six" onClick={() => this.handleInput('six')}>6</button>
+            <button id="add" onClick={() => this.handleInput('add')}>+</button>
+            <button id="one" onClick={() => this.handleInput('one')}>1</button>
+            <button id="two" onClick={() => this.handleInput('two')}>2</button>
+            <button id="three" onClick={() => this.handleInput('three')}>3</button>
+            <button id="decimal" onClick={() => this.handleInput('decimal')}>.</button>
+            <button id="zero" className="jumbo" onClick={() => this.handleInput('zero')}>0</button>
+            <button id="equals" style={{backgroundColor: '#1c4467'}} className="jumbo">=</button>
+          </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
