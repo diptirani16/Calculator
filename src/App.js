f207c9f4457bde9357c52c2e2747581cb2613c3a
Dipti Rani <gdipti642@gmail.com>
2021-08-19 18:07:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,108 +1,155 @@
 import React from 'react';
 import './App.css';
- 
-  let array = [12, '+' , 13, '-', 1, '-', 2, '*', 2, '='];
-  let operand = [];
-  let operator = []
-  let results = '';
-
-  for(var i=0 ; i<array.length; i++){
-     if(isNaN(array[i])){
-      operator.push(array[i])
-     }
-     else{
-      operand.push(array[i])
-     }
-    }
-    
-    for(i=0; i<operator.length; i++){
-      switch(operator[i]){
-        case '+':
-          results = operand[i] + operand[i+1];
-          operand[i+1] = results;
-          break;
-        case '-':
-          results = operand[i] - operand[i+1];
-          operand[i+1] = results;
-          break;
-        case '*':
-          results = operand[i] * operand[i+1];
-          operand[i+1] = results;
-          break;
-        case '/':
-          results = operand[i] / operand[i+1];
-          operand[i+1] = results;
-          break;
-        default:
-          results = operand[i];
-        }
-      }
-      array.push(results);
-      
-      console.log(operand);
-      console.log(operator);
-      console.log(results);
-      console.log(array);
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      input: "0",
+      input: 0,
       formula: [],
-
+      
     }
     this.handleInput = this.handleInput.bind(this);
+    this.currentValue = 0;
+    this.result = []
+    
+    this.Operators = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b
+    }
     this.handleClear = this.handleClear.bind(this);
+    this.Calculate = this.Calculate.bind(this);
   }
 
-  handleInput(e){
-    let newInput = e.target.value;
-    console.log(newInput)
-    this.setState(state => ({
-      input: parseFloat(state.input + newInput.toString()).toString(),
-      formula: parseFloat(state.input + newInput.toString()).toString(),
-      
-    }))
+  handleInput(e) {
+    let value = e.target.value;
+    if(Number.isInteger(parseInt(value)) || parseFloat(value) === '.'){
+      if(!Number.isInteger(this.currentValue)) this.currentValue = 0
+      this.currentValue = parseFloat(this.currentValue + value.toString());
+    }
+    else {
+      if(this.currentValue !== 0 && Number.isInteger(this.currentValue)) this.result = [...this.result, this.currentValue];
+      if(!Number.isInteger(this.result[this.result.length - 1]))
+        this.result[this.result.length - 1] = value
+      else this.result = [...this.result, value]
+      this.currentValue = value
+    }
+    if(value === '=') {
+      this.result = this.Calculate(this.result)
+      this.currentValue = this.result[this.result.length - 1]
+    }
+    console.log(this.currentValue)
+    console.log(this.result)
+    this.setState({ input: this.currentValue, formula: this.result })
 
   }
-  handleClear(){
-    this.setState({ input: 0, formula: '' })
+
+  Calculate(arr) {
+    let currentValue = 0;
+    let currentOperator = '+'
+    arr.map(value => {
+      if(Number.isFinite(value)) {  
+        currentValue = this.Operators[currentOperator](currentValue, value) 
+      } else currentOperator = value
+    })
+    return [...arr, currentValue]
   }
   
 
-  render(){
-  return (
-    <div className="App">
-      <h1>Calculator</h1>
-      <div className="calculator">
+
+  handleClear() {
+    this.currentValue = 0;
+    this.result = []
+    this.setState({ input: 0, formula: '' })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Calculator</h1>
+        <div className="calculator">
           <div className="formulaScreen">{this.state.formula}</div>
           <div className="outputScreen" id="display">{this.state.input}</div>
-          
+
           <div id="number-plate">
             <button id="clear" className="jumbo" onClick={this.handleClear}>AC</button>
             <button id="divide" onClick={this.handleInput} value="/">/</button>
-            <button id="multiply" onClick={this.handleInput} value="x">x</button>
+            <button id="multiply" onClick={this.handleInput} value="*">x</button>
             <button id="seven" onClick={this.handleInput} value="7">7</button>
             <button id="eight" onClick={this.handleInput} value="8">8</button>
             <button id="nine" onClick={this.handleInput} value="9"> 9</button>
-            <button id="subtract" onClick={this.handleInput}  value="-">-</button>
-            <button id="four" onClick={this.handleInput}  value="4">4</button>
-            <button id="five" onClick={this.handleInput}  value="5">5</button>
+            <button id="subtract" onClick={this.handleInput} value="-">-</button>
+            <button id="four" onClick={this.handleInput} value="4">4</button>
+            <button id="five" onClick={this.handleInput} value="5">5</button>
             <button id="six" onClick={this.handleInput} value="6">6</button>
             <button id="add" onClick={this.handleInput} value="+">+</button>
             <button id="one" onClick={this.handleInput} value="1">1</button>
             <button id="two" onClick={this.handleInput} value="2">2</button>
-            <button id="three" onClick={this.handleInput}  value="3">3</button>
-            <button id="decimal" onClick={this.handleInput}  value=".">.</button>
-            <button id="zero" className="jumbo" onClick={this.handleInput}  value="0">0</button>
-            <button id="equals" style={{backgroundColor: '#1c4467'}} className="jumbo">=</button>
+            <button id="three" onClick={this.handleInput} value="3">3</button>
+            <button id="decimal" onClick={this.handleInput} value=".">.</button>
+            <button id="zero" className="jumbo" onClick={this.handleInput} value="0">0</button>
+            <button id="equals" style={{ backgroundColor: '#1c4467' }} onClick={this.handleInput} value="=" className="jumbo">=</button>
           </div>
+        </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 
 export default App;
+
+
+/**
+ *
+function Calculate(arr) {
+  let currentValue = 0;
+  let currentOperator = '+'
+  arr.map(value => {
+    if(Number.isInteger(value)) {
+      currentValue = Operators[currentOperator](currentValue, value)
+    } else currentOperator = value
+  })
+  return [...arr, currentValue]
+}
+
+const Operators = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b
+}
+const arr = [12, '+', 13, '*', '-', 2, '=']
+const output = Calculate(arr)
+console.log(output)
+ */
+
+/*
+function gen(arr) {
+  let currentValue = 0;
+  let result = []
+  arr.map(value => {
+    if(Number.isInteger(value)){
+      currentValue = parseFloat(currentValue + value.toString());
+    }
+    else {
+      if(currentValue !== 0) result = [...result, currentValue];
+      if(!Number.isInteger(result[result.length - 1]))
+        result[result.length - 1] = value
+      else result = [...result, value]
+      currentValue = 0
+    }
+  })
+  return result;
+}
+
+
+const arr = [1,2,3,'+',2,3,'*','-',3, '=']
+
+const output = gen(arr);
+
+console.log(output);
+*/
